bd016454efa4106f3707fc898bd0a3e40ea5636a
Kamil <kdnuriev@gmail.com>
2026-03-21 16:19:24 +0400

    =safe

--- akca/app/account.py
+++ akca/app/account.py
@@ -8,23 +8,27 @@ def account():
 @account.command
 @click.option("-n", "--name", type=str, required=True)
 @click.option("-c", "--currency", type=str, required=True)
-def new(name: str, currency: str):
-    click.echo(f"new acc created: {name=}, {currency=}")
+@click.pass_context
+def new(ctx, name: str, currency: str):
+   ctx.obj.create_account(name, currency) 
 
 @account.command
 @click.option("--id", type=int, required=True)
-def rm(id: int):
+@click.pass_context
+def rm(ctx, id: int):
     click.echo(f"removed account with {id=}")
 
 @account.command
 @click.option("--id", type=int, required=True)
 @click.option("-n", "--name", type=str)
 @click.option("-c", "--currency", type=str)
-def edit(id: int, name: str, currency: str):
+@click.pass_context
+def edit(ctx, id: int, name: str, currency: str):
     click.echo(f"edited account with {id=}, {name=}, {currency=}")
 
 @account.command
-def ls():
+@click.pass_context
+def ls(ctx):
     click.echo("Listing all accounts")
 
 
--- akca/domain/service.py
+++ akca/domain/service.py
@@ -1,5 +1,14 @@
 from akca.db import store
+import click
 
 class Service():
     def __init__(self, store: store.Store):
+        self.store = store
+    def create_account(self, name: str, currency: str) -> int:
+        click.echo(f"new acc created: {name=}, {currency=}")
+    def edit_account(self, id: int, name: str, currency: str) -> int:
+        pass
+    def delete_account(self, id: int):
+        pass
+    def list_account(self):
         pass
